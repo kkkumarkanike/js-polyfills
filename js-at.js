4ee1f20fdf820463af583beb1d693594006d2483
Kanike Kalyan Kumar <37512504+kkkumarkanike@users.noreply.github.com>
2024-01-11 11:01:02 +0530

    added polyfill for js at method

---
+++ js-at.js
@@ -0,0 +1,18 @@
+// this function will take index of an array and returns the value of particular index of an array.
+Array.prototype.myAt = function (index) {
+  // Edge cases: if index is less than the negative of array length or greater than or equal to array length,
+  // will return "undefined"
+  if (index < -this.length || index >= this.length) return undefined;
+
+  let myIndex = index;
+
+  // If passed index is less than 0, then index + array length is accessed (index is counted from the end).
+  if (myIndex < 0) myIndex += this.length;
+
+  return this[myIndex];
+};
+
+const inputArray = [1, 2, 3, 4, 5, 6];
+
+console.log(inputArray.myAt(2));
+console.log(inputArray.myAt(""));
